5595e6954143e83106715f8224e7719aeaa16957
Fernando Perez <fernandoperez0330@gmail.com>
2019-09-12 13:51:15 -0400

    Initial Commit

---
+++ Cognito Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cognito Test
+//
+//  Created by Fernando Perez on 9/12/19.
+//  Copyright © 2019 Pet Safe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
